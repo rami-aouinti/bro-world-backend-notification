3646e8cba966e4163c974b5eaf945a18ba33ff67
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-07-04 21:21:22 +0200

    remove unused functions

--- src/Notification/Application/ApiProxy/UserProxy.php
+++ src/Notification/Application/ApiProxy/UserProxy.php
@@ -37,7 +37,7 @@ readonly class UserProxy
     {
         $response = $this->httpClient->request('GET', "https://bro-world.org/api/v1/user", [
             'headers' => [
-                'Authorization' => 'ApiKey Xdxi9fy6K4LKKkScGy4Hfgybq1B5GSR6Tv8g9Sxa',
+                'Authorization' => 'ApiKey kdSDHn7VvCLj5JmCJo2z9ThGWvviL1gwary8oWKd',
             ],
         ]);
 
