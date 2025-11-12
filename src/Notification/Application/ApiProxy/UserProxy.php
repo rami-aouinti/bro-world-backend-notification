a86336431c698a40302dbbcf3837e32eab345b23
rami aouinti <rami.aouinti@gmail.com>
2025-11-13 00:58:23 +0100

    update package

--- src/Notification/Application/ApiProxy/UserProxy.php
+++ src/Notification/Application/ApiProxy/UserProxy.php
@@ -33,7 +33,7 @@ readonly class UserProxy
     {
         $response = $this->httpClient->request('GET', 'https://bro-world.org/api/v1/user', [
             'headers' => [
-                'Authorization' => 'ApiKey D1CKDKUBb8B7eSytSYchMHq32vLTjvg4gXmHwk4s',
+                'Authorization' => 'ApiKey MfnfDWHw3k3t7J2qFK8CZUg4jQiD4PuWWJpFAm49',
             ],
         ]);
 
